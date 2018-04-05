218b7e8858f9b2493d108d3c412faf90dada306b
Dan Cox <danzabian@gmail.com>
2018-04-05 17:58:37 +0100

    Removing the mongo stuff from traits, moving this to entities

--- engine/trait.go
+++ engine/trait.go
@@ -1,69 +1,45 @@
 package engine
 
-import (
-	"time"
-
-	"gopkg.in/mgo.v2/bson"
-)
-
 type (
 	// Instance is a helper struct that contains all the relevent
 	// information an instance might need
-	Instance struct {
+	Trait struct {
 		Component
 		Client *Client
 	}
 
-	// Trait represents the base contract for a trait
-	// it can be used solely as a trait although it will not
-	// provide any useful functionality.
-	//
-	// The trait struct also provides a useful base for the
-	// mongo integration, if autoSave is enabled traits will
-	// be periodically saved by the server, if autosave is not
-	// enabled, traits can still be saved manually.
-	Trait struct {
-		Id        bson.ObjectId `bson:"_id" json:"-"`
-		ClientId  string        `bson:"clientId" json:"clientId"`
-		CreatedAt time.Time     `bson:"createdAt" json:"createdAt"`
-		UpdatedAt time.Time     `bson:"updatedAt" json:"updatedAt"`
-	}
-
 	// TraitInterface defines the expectations of an instance
 	TraitInterface interface {
+		// SetGM sets the GameManager instance, allowing extra functionality
+		SetGM(*GameManager)
+		// SetClient sets the relevent client to the trait
+		SetClient(*Client)
 		// Register is responsible for setting up an instance before
 		// its connected
-		Register(*Instance)
+		Register()
 		// Connect is called when the instance is bound to the client
-		Connect(*Instance)
+		Connect()
 		// Destroy is called when the instance is destroyed
-		Destroy(*Instance)
+		Destroy()
 	}
 )
 
-// NewInstance creates a new instance
-func NewInstance(GM *GameManager) *Instance {
-	i := &Instance{}
-	i.SetGM(GM)
-	return i
-}
-
 // Register default
-func (t *Trait) Register(i *Instance) {}
+func (t *Trait) Register() {}
 
 // Connect default
-func (t *Trait) Connect(i *Instance) {}
+func (t *Trait) Connect() {}
 
 // Destroy default
-func (t *Trait) Destroy(i *Instance) {}
+func (t *Trait) Destroy() {}
 
 // SetClient sets the connected client
-func (i *Instance) SetClient(c *Client) {
-	i.Client = c
+func (t *Trait) SetClient(c *Client) {
+	t.Client = c
 }
 
 // Handler is an override to create a new event handler,
 // this ensures that an instance can bind to direct event messages
-func (i *Instance) Handler(n string, h EventHandler) {
-	i.Client.RegisterHandler(n, h)
+func (t *Trait) Handler(n string, h EventHandler) {
+	t.Client.RegisterHandler(n, h)
 }
